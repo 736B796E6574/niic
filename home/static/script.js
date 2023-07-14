0ee2c8ba173b4992baf027822ad2974fac83e0a7
Martin McInerney <martin_mcinerney@live.ie>
2023-07-14 20:05:40 +0000

    added prayer times

--- home/static/script.js
+++ home/static/script.js
@@ -1,8 +1,18 @@
 window.onload = function() {
-    document.getElementById('hamburger-icon').addEventListener('click', function() {
-        var mobileNav = document.querySelector('.mobile-nav');
-        mobileNav.classList.toggle('active');
-        console.log("pressed nab toggle")
-    });
+    
+    var images = ['../static/hero__2.jpg', '../static/hero__3.jpg', '../static/hero__4.jpg', '../static/hero__5.jpg'];  // Array containing images
+var index = 0;
+
+function changeImage() {
+  var img = document.getElementById('slider-image');
+  img.src = images[index];
+  index = (index + 1) % images.length; // Move to the next image. Loop back to the first image if we've reached the end.
 }
+
+// Change the image every 5 seconds
+setInterval(changeImage, 5000);
+
+}
+
+
 
